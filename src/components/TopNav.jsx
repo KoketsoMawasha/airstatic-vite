cdba87cbc3580476f7d1ed63822c4c282556268d
Koketso <koketsomawasha@gmail.com>
2024-02-23 02:09:20 +0200

    marketplace coming soon tab

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -5,9 +5,10 @@ function TopNav() {
   return (
     <nav className='flex flex-col justify-center align-middle pt-4 sticky bg-sky-500 top-0'> 
         <img src='/airstatic-v2-svg.svg' alt='airstatic logo' className='w-36 mb-2 bg-contain self-center'/>
-        <ul className='flex justify-evenly align-middle py-4 w-full max-w-sm self-center select-none'>
+        <ul className='grid grid-cols-3 text-center justify-center align-middle pt-4 w-full max-w-sm self-center select-none'>
             <Link to ="/">Home</Link>
             <Link to ="/events">Events</Link>
+            <Link to ="#" className='grid grid-rows-2  '>Marketplace<span className=""><img src="/comin-soon.png" alt="" /></span> </Link>
         </ul>
     </nav>
   )
