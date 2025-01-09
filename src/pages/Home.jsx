3ba39a8d4a23462482d54f2809f8378c4918d999
Koketso <koketsomawasha@gmail.com>
2025-01-09 12:27:26 +0200

    Empty state implementation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,12 @@ import TopNav from '../components/TopNav'
 import HeroBlog from '../components/HeroBlog'
 import Footer from '../components/Footer'
 import supabase from '../services/supabase'
+import VersusSpotlight from '../components/VersusSpotlight'
 
 
 function Home() {
   const [homePosts, setHomePosts] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const getHomePosts = async ()=>{
       let {data, supabaseErr} = await supabase.from('featuredBlogs').select()
@@ -14,6 +16,7 @@ function Home() {
       if(supabaseErr){throw new Error(supabaseErr)}
       if(data){
         setHomePosts(data)
+        setLoading(!loading)
       }
   }
 
@@ -22,20 +25,25 @@ function Home() {
   },[])
 
   return (
-    <div className='bg-neutral-900 w-screen min-h-screen flex flex-col'>
-        <TopNav />
-        <div className='px-4 flex flex-col mt-6 mb-8'>
-          {/* <VersusSpotlight /> */}
-          {homePosts.sort((a,b)=> Number(a.order) - Number(b.order)).map((post)=>{
-          if(post.active){
-            return (
-              <HeroBlog key={post.id} heading={post.heading} img={post.img} summary={post.summary} link={post.linkTo} redirect={post.redirect} buttonTitle={post.ctaTitle}/>
-              )
-          }
-        })}
-        </div>
-        {/* <Footer /> */}
-    </div>
+      <div className='bg-neutral-900 w-screen min-h-screen flex flex-col'>
+          <TopNav />
+          <div className='px-4 flex flex-col mt-6 '>
+            {/* <VersusSpotlight /> */}
+            {loading &&
+      <div className='text-center grid items-center justify-center p-8'>
+        <p className='text-white/70'>Loading...<span className="text-2xl">💐😁</span></p>
+      </div>
+      }
+            {homePosts.sort((a,b)=> Number(a.order) - Number(b.order)).map((post)=>{
+              if(post.active){
+                return (
+                  <HeroBlog key={post.id} heading={post.heading} img={post.img} summary={post.summary} link={post.linkTo} redirect={post.redirect} buttonTitle={post.ctaTitle}/>
+                )
+              }
+            })}
+          </div>
+          {!loading && <Footer />}
+      </div>
   )
 }
 
