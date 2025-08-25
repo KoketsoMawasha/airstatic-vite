38ef492ea7e17150d2a5ed5967504ef763b4925d
Koketso-Mawasha <147575424+Koketso-Mawasha@users.noreply.github.com>
2025-08-25 11:26:13 +0200

    Desktop responsiveness

--- src/components/HeroBlog.jsx
+++ src/components/HeroBlog.jsx
@@ -7,32 +7,38 @@ import { NavLink } from 'react-router-dom'
 
 function HeroBlog({ heading, img = 'https://cdn.midjourney.com/43fc0ccb-7a31-4615-98c9-0708ecb8bf1a/0_2.png', summary, link, redirect, buttonTitle }) {
     const InnerContent = (
-        <>
-            <p className='text-2xl font-heading text-white mb-2'>{heading}</p>
-            <div className="heroImg w-full">
-                <img src={img} alt="Image of featured blog post" className="w-full max-w-sm mb-4 object-cover max-h-[420px] rounded-md"   />
+        <div className='h-[100%] grid content-start'>
+            <div>
+                <p className='text-2xl font-heading text-white mb-2'>{heading}</p>
             </div>
-            <p className='text-stone-400 mb-4'>{summary}</p>
-            {buttonTitle && (<p className='bg-sky-500 font-medium  rounded-md text-center py-2 p-3'>{buttonTitle}</p>)}
-        </>
+            <div className="heroImg ">
+                <img src={img} alt="Image of featured blog post" className="w-full max-w-sm mb-4 object-cover object-bottom max-h-[320px] rounded-md "   />
+                <p className='text-stone-400 mb-4'>{summary}</p>
+            </div>
+            {buttonTitle && (<div className='self-end'>
+                <p className='bg-sky-500 font-medium  rounded-md text-center py-2 p-3'>{buttonTitle}</p>
+            </div>)}
+        </div>
     );
 
     // Render content within a Link if link prop is provided
     if (link) {
         return (
-            <Link to={link}>
-                <div className='max-w-sm flex flex-col justify-center align-middle self-center mb-12 mx-auto'>
+            <div>
+                <Link to={link} className='h-[100%] grid '>
+                <div className='max-w-sm  h-[100%] mb-12 mx-auto'>
                     {InnerContent}
                 </div>
             </Link>
+            </div>
         );
     }
     
     // Render content within an anchor tag if redirect prop is provided
     if (redirect) {
         return (
-            <a href={redirect} target="_blank" rel="noopener noreferrer">
-                <div className='max-w-sm flex flex-col justify-center align-middle self-center mb-12 mx-auto'>
+            <a href={redirect} target="_blank" rel="noopener noreferrer" className='h-[100%] '>
+                <div className='max-w-sm  mb-12 mx-auto'>
                     {InnerContent}
                 </div>
             </a>
@@ -41,7 +47,7 @@ function HeroBlog({ heading, img = 'https://cdn.midjourney.com/43fc0ccb-7a31-461
 
     // Default to rendering the content without a link or redirect
     return (
-        <div className='max-w-sm flex flex-col justify-center align-middle self-center mb-12 mx-auto'>
+        <div className=' align-middle max-w-sm mb-12 mx-auto'>
             {InnerContent}
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,25 +25,27 @@ function Home() {
   },[])
 
   return (
-      <div className='bg-neutral-900 w-screen min-h-screen flex flex-col'>
-          <TopNav />
-          <div className='px-4 flex flex-col mt-6 '>
-            {/* <VersusSpotlight /> */}
-            {loading &&
-      <div className='text-center grid items-center justify-center p-8'>
-        <p className='text-white/70'>Loading...<span className="text-2xl">💐😁</span></p>
-      </div>
+  <div className='bg-neutral-900 w-screen min-h-screen flex flex-col'>
+    <TopNav />
+    <div className='px-4 grid md:grid-cols-2 lg:grid-cols-3 md:gap-x-10 md:gap-y-6 items-start mt-6 max-w-7xl mx-auto'>
+      {/* <VersusSpotlight /> */}
+      {loading &&
+        <div className='text-center grid items-center justify-center p-8'>
+          <p className='text-white/70'>Loading...<span className="text-2xl">💐😁</span></p>
+        </div>
       }
-            {homePosts.sort((a,b)=> Number(a.order) - Number(b.order)).map((post)=>{
-              if(post.active){
-                return (
-                  <HeroBlog key={post.id} heading={post.heading} img={post.img} summary={post.summary} link={post.linkTo} redirect={post.redirect} buttonTitle={post.ctaTitle}/>
-                )
-              }
-            })}
-          </div>
-          {!loading && <Footer />}
-      </div>
+      {homePosts.sort((a,b)=> Number(a.order) - Number(b.order)).map((post)=>{
+        if(post.active){
+          return (
+            <div className="h-[100%]">
+              <HeroBlog key={post.id} heading={post.heading} img={post.img} summary={post.summary} link={post.linkTo} redirect={post.redirect} buttonTitle={post.ctaTitle}/>
+            </div>
+          )
+        }
+      })}
+    </div>
+    {!loading && <Footer />}
+  </div>
   )
 }
 
